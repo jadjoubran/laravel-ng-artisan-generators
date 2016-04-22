1c7f9026b620f164866753c05a1ea7c2d1d5ba6a
Allan Alvarez Ortiz <flick36@gmail.com>
2016-04-22 14:02:20 -0500

    Added option to create page in a custom folder (#26)

--- src/Console/Commands/AngularPage.php
+++ src/Console/Commands/AngularPage.php
@@ -38,12 +38,29 @@ class AngularPage extends Command
      */
     public function handle()
     {
-        $name = $this->argument('name');
+        
+        //Gets the argument and split it into an array
+        $pathArray = explode('/', $this->argument('name'));
+
+        //Gets the last element of the array, that it should be the name of the page
+        $name = end($pathArray);
+
+        //Deletes the last element of the Array (we store it in the variable $name)
+        array_pop($pathArray);
+
+        //Initialize the variable $path with a '/' cause if the array <= 0 it means there's no path, and we should use the default route
+        $path = '/';        
+
+        //We iterate trought the array to concatenate it again, adding always a '/' at the end of each array element
+        foreach ($pathArray as $value){
+            $path = $path.$value.'/';
+        }        
 
         $html = file_get_contents(__DIR__.'/Stubs/AngularPage/page.html.stub');
         $less = file_get_contents(__DIR__.'/Stubs/AngularPage/page.less.stub');
 
-        $folder = base_path(config('generators.source.root')).'/'.config('generators.source.page').'/'.$name;
+        $folder = base_path(config('generators.source.root')).'/'.config('generators.source.page').$path.$name;
+
         if (is_dir($folder)) {
             $this->info('Folder already exists');
 
