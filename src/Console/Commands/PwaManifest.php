1192d1cd87791cd80332c0486a7c9bfc1b1d1301
Jad Joubran <joubran.jad@gmail.com>
2016-06-28 20:59:35 +0300

    finalized pwa:manifest

--- src/Console/Commands/PwaManifest.php
+++ src/Console/Commands/PwaManifest.php
@@ -37,21 +37,21 @@ class PwaManifest extends Command
      */
     public function handle()
     {
-        $manifest = [
-            'icons' => [
-                'src'   => 'img/icon.png',
-                'sizes' => '198x198',
-                'type'  => 'image/png',
-            ],
-        ];
-
         $manifest['lang'] = $this->ask('Enter the language', 'en-us');
         $manifest['name'] = $this->ask('Enter the name of your app', 'Laravel & Angular');
         $manifest['short_name'] = $this->ask('Enter the short name of your app', 'Laravel & Angular');
-        $manifest['display'] = $this->anticipate('Choose a display type?', ['fullscreen', 'standalone', 'minimal-ui', 'browser'], 'standalone');
+        $manifest['display'] = $this->anticipate('Choose a display type? [fullscreen|standalone|minimal-ui|browser]', ['fullscreen', 'standalone', 'minimal-ui', 'browser'], 'standalone');
         $manifest['start_url'] = $this->ask('Enter the start url', '/?homescreen=1');
         $manifest['background_color'] = $this->ask('Enter a background color: ', '#ffffff');
         $manifest['theme_color'] = $this->ask('Enter your theme color', '#0690B7');
+
+        $manifest = [
+            'icons' => [[
+                'src'   => 'img/icon.png',
+                'sizes' => '198x198',
+                'type'  => 'image/png',
+            ]],
+        ];
 
         $output = json_encode($manifest, JSON_PRETTY_PRINT);
 
